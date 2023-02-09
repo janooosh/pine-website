8a00e37636fd7b0c43d29d4c0b3bfe9fd91b6c02
Jan Hähl <jh@cortrium.com>
2023-02-09 21:24:14 +0100

    junge fff

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,9 @@
+import Image from 'next/image'
+
 export function Logo(props) {
   return (
-    <img class="w-auto h-12" src="https://hellopine-public.s3.eu-central-1.amazonaws.com/assets/img/logo/app-logo.svg?color=indigo&shade=500" alt="Your Company" />
+    <>
+              <p>Hello Pine</p>
+    </>
   )
 }
